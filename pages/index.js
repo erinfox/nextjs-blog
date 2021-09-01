1113ac998ed0468848cd7f256b250c60437cdec9
Erin Fox <erinfox@Erins-MBP-CK.attlocal.net>
2021-09-01 16:50:27 -0700

    raw functionality together

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Link from "next/link";
+import { blogPosts } from "../lib/data";
 
 export default function Home() {
   return (
@@ -7,10 +9,23 @@ export default function Home() {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
       <main>
-        <h1>BLOG</h1>
+        <h1>My Blog</h1>
       </main>
+
+      <div>
+        {blogPosts.map((item) => (
+          <div key={item.slug}>
+            <div>
+              <Link href={`/blog/${item.slug}`}>
+                <a>{item.title}</a>
+              </Link>
+            </div>
+            <div>{item.date.toString()}</div>
+            <div>{item.content}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
